b232c82234c23cca92e279b0b25fa46ce4f2b597
lcymu <50705646+lcymu@users.noreply.github.com>
2021-12-22 09:20:33 +0800

    Add files via upload

--- m1.sh
+++ m1.sh
@@ -11,8 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.69.6/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.69.193/g' package/base-files/files/bin/config_generate
+# git clone https://github.com/garypang13/luci-app-dnsfilter.git package/lean/luci-app-dnsfilter
 git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
-rm -rf package/lean/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
