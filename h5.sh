c289ae45201fb861b2a99844eed1682d186657ca
lcymu <50705646+lcymu@users.noreply.github.com>
2022-12-03 09:05:41 +0800

    Update h5.sh

--- h5.sh
+++ h5.sh
@@ -14,5 +14,5 @@
 sed -i 's/192.168.1.1/192.168.69.190/g' package/base-files/files/bin/config_generate
 git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
-rm -rf package/lean/luci-theme-argon
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+#rm -rf package/lean/luci-theme-argon
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
